7f95e9b50bf770b6b3470821719c634d6959f924
MarcoHaber <75615614+marcoshaber99@users.noreply.github.com>
2023-09-01 14:39:03 +0300

    Time: 44 ms (36.3%), Space: 16.2 MB (65.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
-        for _ in range(n - 1):
-            temp = one
-            one = one + two
-            two = temp
-        return one
-        
+        dp = [1, 1] # dp[0] and dp[1] are base cases
+        for i in range(2, n + 1):
+            dp.append(dp[i-1] + dp[i-2])
+        return dp[n]
+
+# test
+s = Solution()
+print(s.climbStairs(2)) # 2
+print(s.climbStairs(6)) # 13
