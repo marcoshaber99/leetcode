c5912783827aaf53eea5229096adf02c9d2edb81
Marco Haber <75615614+marcoshaber99@users.noreply.github.com>
2023-09-09 23:36:08 +0300

    Time: 1601 ms (49.71%), Space: 23.9 MB (88.91%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -2,24 +2,33 @@ class Solution:
 
     
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge(arr, L, M, R):
+
+        
+        def merge(arr, L, M, R): # capitals to be more clear
+
+            # get left and right halfs of the array
             left, right = arr[L:M+1], arr[M+1:R+1]
+
+            # i for the main array and j, k for the two sub-arrays
             i, j, k = L, 0, 0
 
-            while j<len(left) and k< len(right):
+            while j<len(left) and k< len(right): # while loop as long as they are in bounds
                 if left[j] <= right[k]:
                     arr[i] = left[j]
                     j += 1
                 else:
                     arr[i] = right[k]
                     k += 1
+
+                # incrementing the i pointer regardless of which condition executes
                 i += 1
+
             while j<len(left):
-                nums[i] = left[j]
+                arr[i] = left[j]
                 j+=1
                 i+=1
             while k<len(right):
-                nums[i] = right[k]
+                arr[i] = right[k]
                 k+=1
                 i+=1
         
@@ -29,10 +38,19 @@ class Solution:
         def mergeSort(arr, l, r):
             if l==r:
                 return arr
+
+            # find middle
             m = (l+r) // 2
+
+            # run mergeSort on left and right halfa
             mergeSort(arr, l, m)
             mergeSort(arr, m+1, r)
+
+
+            # helper function
             merge(arr, l, m, r)
+
+            #at this point it will be sorted, so return it
             return arr
 
         return mergeSort(nums, 0, len(nums)-1)
